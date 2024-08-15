bbf974c853b4574599ed00b649174a12fcf5d489
tanimh37 <tanimh37@gmail.com>
2024-08-15 13:00:30 +0600

    8/15

---
+++ ev_php_ex/file_class.php
@@ -0,0 +1,43 @@
+<?php
+class file
+{
+    public $fileinfo;
+
+
+    public function __construct($x)
+    {
+        $this->fileinfo = ($x);
+    }
+
+    public function upload()
+    {
+        $info = $this->fileinfo;
+        $name = $info['name'];
+        $tempname = $info['tmp_name'];
+        $filesize = $info['size'];
+        $allowedsize = 512000;
+        $allowedtypes = ["jpg", "png"];
+        $errors = array();
+
+        $fullname = pathinfo($name);
+        $ext = strtolower($fullname['extension']);
+
+        if ($filesize > $allowedsize) {
+            $errors[] = "File size must be 500 kb";
+        }
+
+        if (!in_array($ext, $allowedtypes)) {
+            $errors[] = "$ext is not allow";
+        }
+
+        if (count($errors) > 0) {
+            foreach ($errors as $err) {
+                echo $err . "<br>";
+            }
+        } else {
+            if (move_uploaded_file($tempname, "upload/" . $name)) {
+                echo "uploaded complete";
+            }
+        }
+    }
+}
